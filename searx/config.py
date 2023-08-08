82544ff3faed0eb866985aca5f268eaaf3fab949
Mukund Kulkarni <mukund.kulkarni@wishtreetech.com>
2023-08-08 17:29:22 +0530

    modified feedback loop

--- searx/config.py
+++ searx/config.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 SMTP_DATA = {
     "host": "smtp.office365.com", 
     "port": 587, #STARTTLS Port - 25, 587 or 2587
     "timeout": 30,
     "user": "postmaster@true-search.io",
-    "password": '2postpost@915',
+    "password": os.getenv('SMTP_PASS'),
     "tls":True,
 }
 
