263888a93e36247180b0e78bede4e1bf857c7d3c
LeNovalis <66282712+LeNovalis@users.noreply.github.com>
2023-10-11 09:33:37 -0400

    Update config.py

--- searx/config.py
+++ searx/config.py
@@ -33,5 +33,5 @@ AFFILIATE_ADS = [
     }
 ]
 
-CHARITY_LIST = ["American Red Cross", "Habitat for Humanity", "New York Supportive Housing", "Charity_4"]
+CHARITY_LIST = ["American Red Cross", "Habitat for Humanity", "New York Supportive Housing", "Charity 4"]
 DEFAULT_CHARITY = "American Red Cross"
